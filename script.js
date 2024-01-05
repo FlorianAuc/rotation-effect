2a4501af2c64b0b4c78ccbe32c2e17d1a3f4ce5d
FlorianAuc <florian.aucouturier@gmail.com>
2024-01-05 09:50:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// beta = haut en bas
+// gamma = gauche a droite
+
+const page = document.querySelector(".page");
+const sections = document.querySelectorAll(".section");
+let activeIndex = 0;
+
+window.addEventListener("deviceorientation", (e) => {
+  e.preventDefault();
+  const roll = e.gamma;
+  const pitch = e.beta;
+  console.log(activeIndex);
+
+  if (Math.abs(pitch - 40) < 5 || Math.abs(pitch - 160) < 5) {
+    sections.forEach((section) => {
+      section.classList.remove("active");
+    });
+    activeIndex = activeIndex >= sections.length - 1 ? 0 : activeIndex + 1;
+    sections[activeIndex].classList.add("active");
+  }
+});
